317d1a564194e8bf5e9f57e047deca1787f81f11
Artur Smiechowski <35614427+Dothwile@users.noreply.github.com>
2021-04-11 01:04:55 -0400

    Bones and a Little Meat
    Need to get cross script data to work. Need to add full classification

--- Part_D.py
+++ Part_D.py
@@ -1,56 +1,116 @@
 # %% Imports
 
-import Part_B
-import Part_C
+'''
+
+import Part_B.py
+import Part_C.py
+'''
 import serial
 import argparse
 import numpy as np
 import pyautogui as pyg
+import atexit # To handle closing Serial port on program exit
+
 
 # %% Dummy variables, will figure origin later
 com_port = 'COM3'
+
+gui_scale = 1 # Amount to scale speed of mouse movements
 
 # Note that these all should be loaded in save for sample_buffer
 n_channels = 3
 epoch_size = 200
 sample_buffer = np.zeros((epoch_size, n_channels))
+full_buffer = np.zeros((epoch_size, n_channels))
 
-# %% Live Reading Main Method
+# Variances from other script
+v1 = 0.01
+v2 = 0.01
+v3 = 0.01
+
+# %% Setup Commands and Methods
+
+arduino_data = serial.Serial() # Give the Serial process an appropriate alias
+
+def OpenPort(port, baud):
+    '''OpenPort
+    Arguments-
+    port ~ String of the COM port to open to
+    baud ~ Integer of the data baudrate to use
+    
+    Returns-
+    NONE
+    
+    Opens the Serial port to read in data
+    '''
+    arduino_data.baudrate = baud # Set baudrate
+    arduino_data.port = port # Sets the port to use
+    arduino_data.open()
+
+def OnExit():
+    '''OnExit
+    Arguments-
+    NONE
+    
+    Returns-
+    NONE
+    
+    Closes the COM port
+    '''
+    arduino_data.close()
+    print("Closing EMG Interface")
+
+atexit.register(OnExit) # Registers OnExit method to call on program close
+
+# %% Helper Methods
 
 def Read_EMG_Live():
     ''' Read_EMG_Live
     
-    to add docstring
     
     '''
 
-    with serial.Serial(port=com_port,baudrate=500000) as arduino_data:
+    #with serial.Serial(port=com_port,baudrate=500000) as arduino_data:
     
-        for sample_index in range(sample_buffer.shape[0]): # Iterates over samples in buffer
-            # Extract data string to parse
-            data_string = arduino_data.readline().decode('ascii')
-            # Split into list of strings
-            data_string = data_string.split() # First element is time of sample in ms, rest are sensor values
+    for sample_index in range(sample_buffer.shape[0]): # Iterates over samples in buffer
+        # Extract data string to parse
+        data_string = arduino_data.readline().decode('ascii')
+        # Split into list of strings
+        data_string = data_string.split() # First element is time of sample in ms, rest are sensor values
+        
+        if(len(data_string) >= (n_channels + 1)):
+        
+            # Writes the output of each channel to associate column of data array
+            # Converts to V
+            for channel in range(n_channels):
+                # Write collected data point from each channel to associated position in sd
+                sample_buffer[sample_index, channel] = int(data_string[channel+1])*5.0/1024
+        
+        else:
+            pass # figure out what to do in drop cases
             
-            # Uses short circuit logic and to avoid indexing errors when read line empty or short
-            if(len(data_string) >= (n_channels + 1) and (st[sample_index] >= int(data_string[0]))):
-                st[sample_index] = int(data_string[0])
-            
-                # Writes the output of each channel to associate column of data array
-                # Converts to V
-                for channel in range(n_channels):
-                    # Write collected data point from each channel to associated position in sd
-                    sample_buffer[sample_index, channel] = int(data_string[channel+1])*5.0/1024
-            
-            else: # If data readline is not full, consider it a dropped point and increase time sample index
-                st[sample_index] = int(data_string[0])
-            
-            # Seperate check and loop for plot updates reduces net operations per cycle
-            if((sample_index % 50) == 0):
-                for channel in range(n_channels):                
-                    # Update the buffer?
-                    pass
-                    
+    return sample_buffer # Returns a full epoch
+
+def Classify_EMG(acting_buffer):
+    ''' Classify_EMG
+    Arguments-
+    acting_buffer ~ array of the data to make action decisions on
     
-    # Close the port // Define exit condition?
-    arduino_data.close()
+    Returns-
+    action ~ string of the GUI action to complete
+    
+    Classifies the EMG data of an epoch
+    '''    
+    pass
+
+def Act(action):
+    ''' Act
+    Arguments-
+    action ~ string of the action for GUI to do
+    
+    Returns-
+    NONE
+    
+    Executes a GUI action
+    '''
+    pass    
